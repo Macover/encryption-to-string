45116e8600404fc715a7da509dcd8739d2ec332e
Macover <macover1507@hotmail.com>
2022-05-05 11:20:15 -0500

    structuring the page: Styles edited to responsive

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 "use strict"
 
-const dataInput = document.getElementById('dataInput');
+/*
+   * Finish the validation of the input. 
+   * Add the tags to the array
+   * Extract each array from the stage and calculate their value.
+   * Put all of the values into an new array to show the final result in order to put it into the UI.
+   * Code more styles to the UI.
+*/
 
+
+
+/* CUSTOM METHODS */
 
 const clearInput = () => dataInput.value = "";
-
 
 const sendAlertMessage = (message, bool) => {
     console.log(message)
 }
-
-dataInput.addEventListener("input", e => {
-    const inputValue = e.target.value;
-    if (isNaN(inputValue)) {
-        sendAlertMessage("Error: Solo se aceptan números.");
-        clearInput();
-    }else if(inputValue.length > 2){
-        sendAlertMessage("Error: Rango permitido [1 - 28].");
-    }
-})
+const isANumber = (string) => {
+    let ascii = string.toUpperCase().charCodeAt(0);
+    return ascii >= 48 && ascii <= 57;
+}
